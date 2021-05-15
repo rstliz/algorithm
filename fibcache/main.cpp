3af612e308d3b92303d1b749d374073bc5c0e2fb
ryo310jp <mr.310.ryo@gmail.com>
2021-05-15 17:50:50 +0900

    add fib

---
+++ fibcache/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <climits>
+using namespace std;
+
+int cache[INT_MAX];
+
+int fib(int n) {
+  if (n <= 1) {
+    return n;
+  }
+  if (cache[n]) {
+    return cache[n];
+  }
+  cache[n] = fib(n - 1) + fib(n - 2);
+  return cache[n];
+}
+
+int main() {
+  int n;
+
+  cin >> n;
+  cout << fib(n) << endl;
+}
