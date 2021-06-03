2fcff9f69566c648178dc3be49fe07e9f785eae6
ryo310jp <mr.310.ryo@gmail.com>
2021-06-04 00:05:56 +0900

    Partition

--- partition/main.cpp
+++ partition/main.cpp
@@ -12,7 +12,7 @@ ll n;
 int partition(int A[], int p, int r) {
   int x = A[r];
   int i = p - 1;
-  for (int j = p; j < r - 1; ++j) {
+  for (int j = p; j < r; ++j) {
     if (A[j] <= x) {
       ++i;
       swap(A[i], A[j]);
